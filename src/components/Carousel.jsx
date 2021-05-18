243a6ed3f6b16cca34f8d0f7647ddc1b208d8cc4
gowthamikethavarapu <69476980+gowthamikethavarapu@users.noreply.github.com>
2021-05-18 19:16:11 +0530

    Update Carousel.jsx

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,49 +1,38 @@
 import React  from "react";
-function Carousel(){
+function Carousel(props){
     return(
         <div>
             <div  class="section2">
         <div class="row">
             <div class="col-sm-6 " >
                 <div class="coltext"> 
-                     <h1><i><span><b>W</b>e organise</span></i></h1>
-                     <p>we love events. We love the on-site details, production plans, schedules, deadlines and to do lists that come along with managing an event.  Knowing that every last detail is looked after will allow you to focus on your attendees and stakeholders at the event. Let us look after the rest.</p>
+                     <h1><i><span>{props.heading}</span></i></h1>
+                     <p>{props.matter}</p>
                         <a href="./blog.html"> <button class="btn ">Check now</button></a>
                     </div>
             </div>
             <div class="col-sm-6" >
                 <div class="carousel">
                     <div class="serviceImages">
-                        <div id="carouselExampleControls" class="carousel slide"  data-interval="2000" data-ride="carousel">
+                        <div id={props.id} class="carousel slide"  data-interval="1000" data-ride="carousel">
                          <div class="carousel-inner">
                            <div class="carousel-item active">
-                             <img class="d-block w-100" src="https://images.pexels.com/photos/2959192/pexels-photo-2959192.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="First slide"/>
+                             <img class="d-block w-100" src={props.img1} alt="First slide"/>
                            </div>
                            <div class="carousel-item">
-                             <img class="d-block w-100" src="https://images.pexels.com/photos/3748176/pexels-photo-3748176.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"  alt="Secondslide"  />
+                             <img class="d-block w-100" src={props.img2}  alt="Secondslide"  />
                            </div>
                            <div class="carousel-item">
-                             <img class="d-block w-100" src="https://images.pexels.com/photos/1857157/pexels-photo-1857157.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Thirdslide" />
+                             <img class="d-block w-100"src={props.img3} alt="Thirdslide" />
                            </div>
                            <div class="carousel-item">
-                            <img class="d-block w-100" src="https://images.pexels.com/photos/1190297/pexels-photo-1190297.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Fourthslide"/>
-                          </div>
-                          <div class="carousel-item">
-                            <img class="d-block w-100" src="https://images.pexels.com/photos/1708936/pexels-photo-1708936.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Fifthslide" />
+                            <img class="d-block w-100" src={props.img4} alt="Fourthslide"/>
                           </div>
                           
-                        
-                         
+                          
                          </div>
                          
-                         <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                           <span class="sr-only">Previous</span>
-                         </a>
-                         <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                           <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                           <span class="sr-only">Next</span>
-                         </a>
+                         
                        </div>
                     </div>
 
@@ -51,95 +40,7 @@ function Carousel(){
         </div>
      </div> 
      </div>
-     <div class="section2" >
-        <div class="row">
-            
-            <div class="col-sm-6" >
-                <div class="carousel">
-                    <div class="serviceImages">
-                        <div id="carouselExampleControls2" class="carousel slide"  data-interval="2000" data-ride="carousel">
-                         <div class="carousel-inner">
-                           <div class="carousel-item active">
-                             <img class="d-block w-100" src="https://images.pexels.com/photos/2959192/pexels-photo-2959192.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="First slide"/>
-                           </div>
-                           <div class="carousel-item">
-                             <img class="d-block w-100" src="https://images.pexels.com/photos/6479590/pexels-photo-6479590.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Second slide"/>
-                           </div>
-                           <div class="carousel-item">
-                             <img class="d-block w-100" src="https://images.pexels.com/photos/169198/pexels-photo-169198.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Third slide"/>
-                           </div>
-                           <div class="carousel-item">
-                            <img class="d-block w-100" src="https://images.pexels.com/photos/7666527/pexels-photo-7666527.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Third slide"/>
-                          </div>
-                         </div>
-                         
-                        </div>
-                         
-                        <a class="carousel-control-prev" href="#carouselExampleControls2" role="button" data-slide="prev">
-                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                          <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#carouselExampleControls2" role="button" data-slide="next">
-                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                          <span class="sr-only">Next</span>
-                        </a>
-                       </div>
-                    </div>
-
-                </div>
-                <div class="col-sm-6 " >
-                    <div class="coltext"> 
-                         <h1><i><span><b>S</b>ervices</span></i></h1>
-                         <p> Bigday events designs and executes special occasions shaped to meet the client's style and personality. From large, branded corporate events to intimate weddings and everything in between, Her team provide unique insights that will make your big day unforgettable.</p>
-                          <a href="./price.html"> <button class="btn ">Book now</button></a>
-                        </div>
-                </div>
-        </div>
-     </div> 
-     <div className="section2" style={{paddingBottom: "150px"}}>
-     <div class="row">
-            <div class="col-sm-6 " >
-                <div class="coltext"> 
-                     <h1><i><span><b>O</b>ur network</span></i></h1>
-                     <p>we love events. We love the on-site details, production plans, schedules, deadlines and to do lists that come along with managing an event.  Knowing that every last detail is looked after will allow you to focus on your attendees and stakeholders at the event. Let us look after the rest.</p>
-                        <a href="./blog.html"> <button class="btn ">Check now</button></a>
-                    </div>
-            </div>
-           
-            <div class="col-sm-6" >
-                <div class="carousel">
-                    <div class="serviceImages">
-                        <div id="carouselExampleControls3" class="carousel slide"  data-interval="2000" data-ride="carousel">
-                         <div class="carousel-inner">
-                           <div class="carousel-item active">
-                             <img class="d-block w-100" src="https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="First slide"/>
-                           </div>
-                           <div class="carousel-item">
-                             <img class="d-block w-100" src="https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"  alt="Secondslide"  />
-                           </div>
-                           <div class="carousel-item">
-                             <img class="d-block w-100" src="https://images.pexels.com/photos/408503/pexels-photo-408503.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Thirdslide" />
-                           </div>
-                          
-                        
-                         
-                         </div>
-                         
-                         <a class="carousel-control-prev" href="#carouselExampleControls3" role="button" data-slide="prev">
-                           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                           <span class="sr-only">Previous</span>
-                         </a>
-                         <a class="carousel-control-next" href="#carouselExampleControls3" role="button" data-slide="next">
-                           <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                           <span class="sr-only">Next</span>
-                         </a>
-                       </div>
-                    </div>
-
-                </div>
-        </div>
-     </div> 
-     </div>
+  
      </div>
     
     );
